89f60e904064023aea2bc04afe5dcf6c51f11b32
EC2 Default User <ec2-user@ip-172-31-46-156.ec2.internal>
2022-08-31 01:34:31 +0000

    Task 5

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=millicentmalinga/project-4-ml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run project-4-ml --image=$dockerpath --port=80 --labels app=project-4-ml
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-foward project-4-ml 8000:80
 
